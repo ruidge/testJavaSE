06e001f0d8a379ec1877cc52f1b25817add15b47
zhangrui <zhangrui@jijiankang.cn>
2016-06-29 18:25:25 +0800

    gson update

--- src/com/ruidge/test/TestGson.java
+++ src/com/ruidge/test/TestGson.java
@@ -2,6 +2,7 @@ package com.ruidge.test;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
@@ -24,5 +25,86 @@ public class TestGson {
         Map<String, String> newMap = gson.fromJson(json, typeOfHashMap); // This type must match TypeToken
         System.out.println(newMap.get("one"));
         System.out.println(newMap.get("two"));
+
+        String resultStr = "{\n" +
+                "\t\"resultCode\": \"123\",\n" +
+                "\t\"resultMsg\": \"successadfasdfs\",\n" +
+                "\t\"result\": [{\n" +
+                "\t\t\"normalRisk\": 15,\n" +
+                "\t\t\"id\": 106,\n" +
+                "\t\t\"time\": \"2016-06-28\",\n" +
+                "\t\t\"userName\": \"梁新姣\",\n" +
+                "\t\t\"highRisk\": 25,\n" +
+                "\t\t\"lowRisk\": 10,\n" +
+                "\t\t\"productName\": \"记健康基因检测\",\n" +
+                "\t\t\"bindStatus\": 3,\n" +
+                "\t\t\"status\": 1\n" +
+                "\t}, {\n" +
+                "\t\t\"normalRisk\": 15,\n" +
+                "\t\t\"id\": 44,\n" +
+                "\t\t\"time\": \"2016-06-28\",\n" +
+                "\t\t\"userName\": \"我叫新报告\",\n" +
+                "\t\t\"highRisk\": 25,\n" +
+                "\t\t\"lowRisk\": 10,\n" +
+                "\t\t\"productName\": \"记健康基因检测\",\n" +
+                "\t\t\"bindStatus\": 5,\n" +
+                "\t\t\"status\": 4\n" +
+                "\t}, {\n" +
+                "\t\t\"normalRisk\": 15,\n" +
+                "\t\t\"id\": 68,\n" +
+                "\t\t\"time\": \"2016-06-27\",\n" +
+                "\t\t\"userName\": \"梁新姣\",\n" +
+                "\t\t\"highRisk\": 25,\n" +
+                "\t\t\"lowRisk\": 10,\n" +
+                "\t\t\"productName\": \"记健康基因检测\",\n" +
+                "\t\t\"bindStatus\": 5,\n" +
+                "\t\t\"status\": 2\n" +
+                "\t}, {\n" +
+                "\t\t\"normalRisk\": 15,\n" +
+                "\t\t\"id\": 66,\n" +
+                "\t\t\"time\": \"2016-06-27\",\n" +
+                "\t\t\"userName\": \"梁新姣\",\n" +
+                "\t\t\"highRisk\": 25,\n" +
+                "\t\t\"lowRisk\": 10,\n" +
+                "\t\t\"productName\": \"记健康基因检测\",\n" +
+                "\t\t\"bindStatus\": 5,\n" +
+                "\t\t\"status\": 3\n" +
+                "\t}, {\n" +
+                "\t\t\"normalRisk\": 15,\n" +
+                "\t\t\"id\": 69,\n" +
+                "\t\t\"time\": \"2016-06-27\",\n" +
+                "\t\t\"userName\": \"梁新姣\",\n" +
+                "\t\t\"highRisk\": 25,\n" +
+                "\t\t\"lowRisk\": 10,\n" +
+                "\t\t\"productName\": \"记健康基因检测\",\n" +
+                "\t\t\"bindStatus\": 5,\n" +
+                "\t\t\"status\": 5\n" +
+                "\t}]\n" +
+                "}";
+
+        ChildResult result = gson.fromJson(resultStr, ChildResult.class);
+
+        System.out.println(result.toString());
+
+    }
+
+
+    class Result {
+        String resultCode;
+        String resultMsg;
+
+    }
+
+    class ChildResult extends Result {
+        JsonArray result;
+
+        @Override
+        public String toString() {
+            return "ChildResult{" +
+                    "resultCode=" + resultCode +
+                    "resultMsg=" + resultMsg +
+                    "result=" + result +
+                    '}';
+        }
     }
 }
